7f4c4f704c288d4c06c037dc2e07adff13b5c186
yukiny0811 <yukiny0811@gmail.com>
2024-05-22 17:08:26 +0900

    Initial Commit

---
+++ VideoFilterTest/VideoFilterTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFilterTestApp.swift
+//  VideoFilterTest
+//
+//  Created by Yuki Kuwashima on 2024/05/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoFilterTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
